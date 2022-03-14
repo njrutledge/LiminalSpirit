b71ce38ae57afa104426d9a05ee7cbd27c17cadc
Chenyu Wei <chenyuwei@Chenyus-MacBook-Pro.local>
2022-03-14 16:47:27 -0400

    did not use those files

--- release/source/PlatformSet.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  PlatformSet.cpp
-//  LiminalSpirit (Mac)
-//
-//  Created by Chenyu Wei on 3/13/22.
-//  Copyright © 2022 Game Design Initiative at Cornell. All rights reserved.
-//
-
-#include "PlatformSet.hpp"
