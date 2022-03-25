b74f4d243eb6679ae87774ff81152ee1071a6d40
Jonathan Gomez <jdg274@cornell.edu>
2022-03-25 14:04:35 -0400

    Created Glutton files

---
+++ release/source/Glutton.cpp
@@ -0,0 +1,9 @@
+//
+//  Glutton.cpp
+//  LiminalSpirit
+//
+//  Created by Jonathan Gomez on 3/25/22.
+//  Copyright © 2022 Game Design Initiative at Cornell. All rights reserved.
+//
+
+#include "Glutton.hpp"
---
+++ release/source/Glutton.hpp
@@ -0,0 +1,14 @@
+//
+//  Glutton.hpp
+//  LiminalSpirit
+//
+//  Created by Jonathan Gomez on 3/25/22.
+//  Copyright © 2022 Game Design Initiative at Cornell. All rights reserved.
+//
+
+#ifndef Glutton_hpp
+#define Glutton_hpp
+
+#include <stdio.h>
+
+#endif /* Glutton_hpp */
