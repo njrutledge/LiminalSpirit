513118c733ced5934a6a5241627f738884624efd
Chenyu Wei <chenyuwei@Chenyus-MacBook-Pro.local>
2022-04-18 17:39:13 -0400

    nothing change from spawner branch, to save time on merging

---
+++ release/source/Spawner.cpp
@@ -0,0 +1,26 @@
+//
+//  Spawner.cpp
+//  LiminalSpirit
+//
+//  Created by Chenyu Wei on 4/18/22.
+//  Copyright © 2022 Game Design Initiative at Cornell. All rights reserved.
+//
+
+#include "Spawner.hpp"
+
+EnemyProperties SPAWNER_PROPS{
+    /** Starting health of the spawner */
+    10,
+    /** Vertical speed of the spawner */
+    0.0f,
+    /** Horizontal speed of the spawner*/
+    0.0f,
+    /** Cooldown for attack(s) in seconds*/
+    2,
+    /** Attack radius of the spawner*/
+    0.0f,
+    /** Density of the spawner*/
+    1.0f,
+    /** Name of the spawner*/
+    "Spawner",
+};
