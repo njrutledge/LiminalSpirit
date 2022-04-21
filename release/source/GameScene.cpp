159efa529e7c910a4dca5a81f7a48a072423d9f9
ssp92 <ssp92@cornell.edu>
2022-04-21 01:06:18 -0400

    tweak projectiles

--- release/source/GameScene.cpp
+++ release/source/GameScene.cpp
@@ -247,7 +247,7 @@ bool GameScene::init(const std::shared_ptr<cugl::AssetManager>& assets, const st
     // TODO this init might be wrong, Nick had _scale/2.0f
     _pMeleeTexture = _assets->get<Texture>(PATTACK_TEXTURE);
     _attacks = std::make_shared<AttackController>();
-    _attacks->init(_scale, 1.5, cugl::Vec2::UNIT_Y, cugl::Vec2(0, 0.5), 0.5, 1, 0.5, 0.1, DEFAULT_WIDTH, DEFAULT_HEIGHT);
+    _attacks->init(_scale, _scale * 1.5, 1.5, cugl::Vec2(0, 1.25), cugl::Vec2(0, 0.5), 0.5, 1, 0.35, 0.1, DEFAULT_WIDTH, DEFAULT_HEIGHT);
     _dashTime = 0;
     _dashXVel = 0;
     _dashYVel = 0;
