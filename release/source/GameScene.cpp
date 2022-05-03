d394534477cb86ceaded9b17f94670e213051338
ssp92 <ssp92@cornell.edu>
2022-05-02 20:26:13 -0400

    fix range arm artifact, charged projectile kinda

--- release/source/GameScene.cpp
+++ release/source/GameScene.cpp
@@ -1309,11 +1309,12 @@ void GameScene::updateSwipesAndAttacks(float timestep) {
             }
         }
         else if (attackType == AttackController::Type::p_exp_package) {
-            std::shared_ptr<Texture> attackTexture = _assets->get<Texture>("player_projectile");//this string is wrong
-            attackSprite = scene2::SpriteNode::alloc(attackTexture, 1, 1);//this is wrong for sprite sheet
-            attackSprite->setScale(.85f * (*it)->getRadius());
+            std::shared_ptr<Texture> attackTexture = _assets->get<Texture>("player_charged_projectile");
+            attackSprite = scene2::SpriteNode::alloc(attackTexture, 1, 5);
+            attackSprite->setAnchor(0.4, 0.55);
+            attackSprite->setScale(.65f * (*it)->getRadius());
+            dynamic_pointer_cast<scene2::SpriteNode>(attackSprite)->setFrame(0);
             attackSprite->setAngle((*it)->getAngle() * M_PI / 180);
-            attackSprite->setColor(Color4::RED);
             attackSprite->setPriority(3);
             _rangedArm->setLastType(Glow::MeleeState::first);
             _player->setRangedAttackRight(_player->isFacingRight());
