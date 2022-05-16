127b068d5bc112286cf3dca588bbedeac2a7c7e1
Alex Lee <cal368@cornell.edu>
2022-05-16 15:59:14 -0400

    New Forest Levels, Also slight variance to enemy speeds and range (to reduce stacking)

--- release/source/LevelSelectScene.hpp
+++ release/source/LevelSelectScene.hpp
@@ -34,7 +34,7 @@
 
 #define CAVE_MAXLEVELS 9
 #define SHROOM_MAXLEVELS 9
-#define FOREST_MAXLEVELS 7
+#define FOREST_MAXLEVELS 8
 /**
  * Class representing the Home screen. It links to world select and options menu.
  */
