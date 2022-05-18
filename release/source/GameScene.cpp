c2cb1309267f964f15e18910ce432ddb6ae770e9
Alex Lee <cal368@cornell.edu>
2022-05-18 17:31:38 -0400

    Removing reset function, GameScene now disposes and reinitializes
    Also bandaid fixed a ParticleNode bug

--- release/source/GameScene.cpp
+++ release/source/GameScene.cpp
@@ -98,6 +98,7 @@ bool GameScene::init(const std::shared_ptr<cugl::AssetManager> &assets, const st
 {
     _back = false;
     _levelselect = false;
+    _restart = false;
     _step = false;
     _winInit = true;
     _lose = false;
@@ -460,9 +461,7 @@ bool GameScene::init(const std::shared_ptr<cugl::AssetManager> &assets, const st
                                     {
             // Only quit when the button is released
             if (!down) {
-                _lose = false;
-                reset();
-                _player->markRemoved(false);
+                _restart = true;
             } });
     _loseRestartButton->setScale(.4 * buttonScale);
 
@@ -3915,113 +3914,6 @@ void GameScene::buildScene(std::shared_ptr<scene2::SceneNode> scene)
 
     // We can only activate a button AFTER it is added to a scene
     _pauseButton->activate();
-}
-
-/**
- * Resets the status of the game so that we can play again.
- */
-void GameScene::reset()
-{
-    _input.reset();
-    _swipes.reset();
-    _tilt.reset();
-    _collider.reset();
-    if (_worldnode)
-    {
-        for (std::shared_ptr<scene2::SceneNode> s : _worldnode->getChildren())
-        {
-            if (s->getTag() == 100)
-            {
-                s->dispose();
-            }
-        };
-    }
-
-    auto ac_it = _attacks->_current.begin();
-    while (ac_it != _attacks->_current.end())
-    {
-        // int log1 = _world->getObstacles().size();
-        cugl::physics2::Obstacle *obj = dynamic_cast<cugl::physics2::Obstacle *>(&**ac_it);
-        _world->removeObstacle(obj);
-        _worldnode2->removeChild(obj->_node);
-
-        // int log2 = _world->getObstacles().size();
-        ac_it = _attacks->_current.erase(ac_it);
-    }
-    auto ap_it = _attacks->_current.begin();
-    while (ap_it != _attacks->_current.end())
-    {
-        // int log1 = _world->getObstacles().size();
-        cugl::physics2::Obstacle *obj = dynamic_cast<cugl::physics2::Obstacle *>(&**ap_it);
-        _world->removeObstacle(obj);
-        _worldnode2->removeChild(obj->_node);
-
-        // int log2 = _world->getObstacles().size();
-        ac_it = _attacks->_current.erase(ap_it);
-    }
-    //_attacks->reset();  Shouldn't be needed now
-    // Does nothing right now
-    _ai.reset();
-
-    // Reset player position & health & other member fields
-    Vec2 playerPos = PLAYER_POS;
-    _player->reset(playerPos);
-    _player->getSceneNode()->setVisible(true);
-    _player->getSceneNode()->setColor(Color4::WHITE);
-    _rangedArm->getSceneNode()->setVisible(true);
-    _meleeArm->getSceneNode()->setVisible(true);
-    scene2::SpriteNode *sprite = dynamic_cast<scene2::SpriteNode *>(_player->getSceneNode().get());
-    sprite->setFrame(12);
-    _prevFrame = 12;
-    _dashTime = 0;
-    _dashXVel = 0;
-    _dashYVel = 0;
-    _cancelDash = false;
-    // Remove all enemies
-    auto eit = _enemies.begin();
-    while (eit != _enemies.end())
-    {
-        cugl::physics2::Obstacle *obj = dynamic_cast<cugl::physics2::Obstacle *>(&**eit);
-        cugl::physics2::Obstacle *glowObj = dynamic_cast<cugl::physics2::Obstacle *>(&*(*eit)->getGlow());
-        _world->removeObstacle(glowObj);
-        _worldnode2->removeChild(glowObj->_node);
-        _world->removeObstacle(obj);
-        _worldnode2->removeChild(obj->_node);
-
-        eit = _enemies.erase(eit);
-    }
-
-    _spawners.clear();
-    // Reset wave spawning
-    _timer = 0.0f;
-    _nextWaveNum = 0;
-    _spawnParticlesDone = false;
-    auto spawnTime = _constants->get("spawn_times");
-    for (int i = 0; i < _numWaves; i++)
-    {
-        _spawn_times[i] = spawnTime->get(i)->asFloat();
-    }
-    _spawnerCount = 0;
-    _spawner_ind = -1;
-    int index = 0;
-    for (auto it = _spawner_enemy_types.begin(); it != _spawner_enemy_types.end(); ++it)
-    {
-        if (_living_spawners[index])
-        {
-            _living_spawners[index] = 0;
-        }
-        for (auto i : (*it))
-        {
-            _spawner_enemy_types[index][(i.first)].timer = 10.0f;
-            _spawner_enemy_types[index][(i.first)].current_count = 0;
-        }
-        index++;
-    }
-    _tutorial = _initTutorial;
-    _tutorialInd = 0;
-    _tutorialTimer = TUTORIAL_INIT_TIMER;
-    _tutorialActionDone = false;
-    _endText = nullptr;
 }
 
 /**
